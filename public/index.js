2cc8c21b4c2446ab27d144224ca6984a367ae0d3
maolei <ojquery@163.com>
2022-02-19 21:34:49 +0800

    component

--- public/index.js
+++ public/index.js
@@ -1,17 +1,180 @@
+// var a = [1, 2]
+// a.push(3)
+// console.log(a);
+Vue.component('hdSlide', {
+    template: '<h1>houdun.com</h1>'
+})
+
+var lists = {
+
+}
+
+var hdNews = {
+    template: "#hdNews",
+    props: {
+        showDelButton: {
+            type: [Boolean, Number],
+            default: true
+        },
+        lists: {
+            type: Array
+        },
+    },
+    data() {
+        return {
+            // news: [
+            //     { title: 'cms' },
+            //     { title: 'php' },
+            // ]
+        }
+    },
+    methods: {
+        delNews(k) {
+            this.lists.splice(k, 1);
+        },
+    },
+}
 
 var app = new Vue({
     el: "#hdcms",
+    components: {
+        hdNews
+    },
+    watch: {
+        'field.click': function (n, o) {
+            console.log(typeof (n))
+        },
+        'field.title': function (n, o) {
+            console.log(n.length)
+        }
+    },
+    computed: {
+        // status() {
+        //     if (this.type == 'all') {
+        //         return this.user;
+        //     } else {
+        //         // return this.user.filter(function (params) {
+        //         //     return params.gender == app.type;
+        //         // });
+        //         return this.user.filter((v) => {
+        //             return v.gender == this.type;
+        //         });
+        //     }
+        // }
+    },
     methods: {
+        handler() {
+            console.log('php');
+        },
+        keyup() {
+            console.log("keyup")
+        },
+        box() {
+            console.log('box');
+        },
+        link() {
+            console.log('links');
+        },
+        post(num) {
+            console.log(num);
+        },
+        search() {
+            this.comments = this.comments.filter((item) => {
+                var reg = new RegExp(this.search_content, 'i');
+                return reg.test(item.content);
+            })
+        },
         changeStatus: function (item, status) {
             item.status = status;
+        },
+        remove(k) {
+            this.comments.splice(k, 1);
+        },
+        push(type) {
+            var id = this.comments.length + 1;
+            content = { id: id, content: this.current_content }
+            switch (type) {
+                case "pre":
+                    this.comments.unshift(content);
+                    break;
+                case "end":
+                    this.comments.push(content);
+                    break;
+            }
+            this.current_content = '';
+        },
+        del(type) {
+            switch (type) {
+                case 'pre':
+                    this.comments.shift();
+                    break;
+                case 'end':
+                    this.comments.pop();
+                    break;
+            }
+        },
+        sort() {
+            this.comments.sort(function (a, b) {
+                return a.id - b.id;
+            })
+        },
+        reverse() {
+            this.comments.reverse();
         }
     },
     data: {
-        user: {
-            name: 'name',
-            age: 20,
-            gender: 'male'
-        }
+        news: [
+            { title: 'cms' },
+            { title: 'php' },
+        ],
+        type: [
+            { name: 'draft', title: 'Draft' },
+            { name: 'send', title: 'Send' },
+        ],
+        flag: [
+            { name: 'hot', title: 'Hot' },
+            { name: 'recommend', title: 'Recommend' },
+        ],
+        category: [
+            { cid: 1, title: 'php' },
+            { cid: 2, title: 'java' },
+            { cid: 3, title: 'python' },
+            { cid: 4, title: 'go' },
+        ],
+        field: {
+            title: 'title',
+            click: 100,
+            url: "houdun.com",
+            description: 'desc',
+            type: 'draft',
+            flag: [],
+            cid: ''
+        },
+        style: {
+            border: 'solid 2px red',
+            height: '300px'
+        },
+        time: new Date(),
+        search_content: '',
+        current_content: '',
+        comments: [
+            { id: 2, content: 'houdun2' },
+            { id: 4, content: 'houdun4' },
+            { id: 1, content: 'houdun1' },
+            { id: 3, content: 'houdun3' },
+        ]
+        // type: 'all',
+        // user: [
+        //     { name: 'name', gender: 'male' },
+        //     { name: 'name', gender: 'male' },
+        //     { name: 'name', gender: 'female' },
+        //     { name: 'name', gender: 'male' },
+        // ]
+        // user: {
+        //     name: 'name',
+        //     age: 20,
+        //     gender: 'male'
+        // }
         // news: [
         //     { id: 1, title: "houdun" },
         //     { id: 2, title: "houdunwang" }
